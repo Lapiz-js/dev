d6d1ddd1e06ee0453254dea2d2d51519dbc0d392
Adam Colton <adamcolton@gmail.com>
2016-05-21 21:53:00 -0400

    added static html unit test files, better docs and test coverage

--- build.php
+++ build.php
@@ -54,6 +54,44 @@ include 'tools/JShrink/src/JShrink/Minifier.php';
     }
 
     new IndexDocs();
+
+    $test_src_html = file_get_contents('tools/unitTestHeader.html');
+    $test_build_html = $test_src_html;
+    $test_min_html = $test_src_html;
+
+    foreach($lapiz->projects as $projectName => $project){
+      $test_build_html .= "    <script type='text/javascript' src='build/$projectName.js'></script>\n";
+      $test_min_html .= "    <script type='text/javascript' src='build/min/$projectName.min.js'></script>\n";
+      foreach ($project['inits'] as $script) {
+        $test_src_html .= "    <script type='text/javascript' src='$script'></script>\n";
+      }
+    }
+    foreach($lapiz->projects as $projectName => $project){
+      foreach ($project['src'] as $script) {
+        $test_src_html .= "    <script type='text/javascript' src='$script'></script>\n";
+      }
+    }
+    foreach(['testing/test.js', 'testing/ui.js'] as $script){
+      $test_src_html .= "    <script type='text/javascript' src='$script'></script>\n";
+      $test_build_html .= "    <script type='text/javascript' src='$script'></script>\n";
+      $test_min_html .= "    <script type='text/javascript' src='$script'></script>\n";
+    }
+
+    foreach($lapiz->projects as $projectName => $project){
+      foreach ($project['tests'] as $script) {
+        $test_src_html .= "    <script type='text/javascript' src='$script'></script>\n";
+        $test_build_html .= "    <script type='text/javascript' src='$script'></script>\n";
+        $test_min_html .= "    <script type='text/javascript' src='$script'></script>\n";
+      }
+    }
+    $footer = "  </body>\n</html>";
+    $test_src_html .= $footer;
+    $test_build_html .= $footer;
+    $test_min_html .= $footer;
+
+    file_put_contents('test.src.html', $test_src_html);
+    file_put_contents('test.build.html', $test_build_html);
+    file_put_contents('test.min.html', $test_min_html);
     ?>
     </div>
   </body>
